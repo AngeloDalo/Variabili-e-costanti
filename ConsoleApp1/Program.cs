f5d18ce62369d9381943fb19056efba91444511f
Angelo <Angelo@DESKTOP-6JKRPIT>
2023-04-26 17:39:50 +0200

    costant

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,21 @@ namespace Corso
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
+            //System.Console.WriteLine("Hello World");
+            string nome = "Angelo";
+            string cognome;
+            cognome = "DAlo";
+
+            //possiamo riassegnare le varibaili, non per le costanti
+            nome = "Angelo2";
+
+            int eta = 23;
+
+            Console.WriteLine(nome + " " + eta);
+            Console.Read();
+
+            //COSTANTI
+            const string genere = "uomo";
         }
     }
 }
